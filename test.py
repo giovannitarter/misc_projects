c9243ae182a406c55938b90b61dae12bc828f12c
Giovanni Tarter <giovanni.tarter@gmail.com>
2019-08-13 15:02:08 +0200

    backup

--- test.py
+++ test.py
@@ -62,14 +62,23 @@ def transpose(start, end, header_row, line_to_price):
     return res
 
 
+def print_matrix(mat):
+    for r in mat:
+        print(r)
+        price_sum = 0
+        for price in mat[r]:
+            print(" > {}".format(price))
+            price_sum = price_sum + price[1][3]
+        print("sum: {}".format(price_sum))
+
+
+
 if __name__ == "__main__":
     
     book = xlrd.open_workbook(sys.argv[1])
     print("The number of worksheets is {0}".format(book.nsheets))
     print("Worksheet name(s): {0}".format(book.sheet_names()))
     sh = book.sheet_by_index(6)
-    
-
 
     int_cat, ext_cat = parse_categories(sh)
     
@@ -154,55 +163,98 @@ if __name__ == "__main__":
     print(tot_est)
     
 
-    for line in line_to_price:
-        print("{} : {}".format(line, line_to_price[line]))
-
-
-    print(all_cat)
-
-    cat_to_prices = {}
-    for cat in int_cat:
-        prices_list = cat_to_prices.get(cat, [])
-        cat_to_prices[cat] = prices_list
+#    for line in line_to_price:
+#        print("{} : {}".format(line, line_to_price[line]))
+#
+#
+#    print(all_cat)
+#
+#    cat_to_prices = {}
+#    for cat in int_cat:
+#        prices_list = cat_to_prices.get(cat, [])
+#        cat_to_prices[cat] = prices_list
+#        
+#        for idx, cell in enumerate(sh.col(cat)):
+#            if cell.ctype == 2:
+#                if idx in line_to_price.keys():
+#                    prices_list.append(line_to_price[idx])
+#    
+#    for cat in ext_cat:
+#        prices_list = cat_to_prices.get(cat, [])
+#        cat_to_prices[cat] = prices_list
+#        
+#        for idx, cell in enumerate(sh.col(cat)):
+#            if cell.ctype == 2:
+#                if idx in line_to_price.keys():
+#                    prices_list.append(line_to_price[idx])
+#
+#
+#    for cat in cat_to_prices:
+#        print("")
+#        print(all_cat[cat])
+#        print(sum([ x[1][3] for x in cat_to_prices[cat]]))
+#
+#
+#    cat_contrib = sh.row(1)
+#    for idx, c in enumerate(cat_contrib):
+#        print("{} -> {}".format(idx, c.value))
         
-        for idx, cell in enumerate(sh.col(cat)):
-            if cell.ctype == 2:
-                if idx in line_to_price.keys():
-                    prices_list.append(line_to_price[idx])
-    
-    for cat in ext_cat:
-        prices_list = cat_to_prices.get(cat, [])
-        cat_to_prices[cat] = prices_list
-        
-        for idx, cell in enumerate(sh.col(cat)):
-            if cell.ctype == 2:
-                if idx in line_to_price.keys():
-                    prices_list.append(line_to_price[idx])
-
-
-    for cat in cat_to_prices:
-        print("")
-        print(all_cat[cat])
-        print(sum([ x[1][3] for x in cat_to_prices[cat]]))
-
-
-    cat_contrib = sh.row(1)
-    for idx, c in enumerate(cat_contrib):
-        print("{} -> {}".format(idx, c.value))
-        
-    
-   
 
     #tot_gio = #row 30
     #tot_ari = #row 31
     
-    #row 32 - 37
+    #esterno
+    res = transpose(16, 21, 0, line_to_price)
+    print_matrix(res)
+    print("")
+    
+    #interno
+    res = transpose(22, 28, 0, line_to_price)
+    print_matrix(res)
+    print("")
+    
+    #arianna esterno tot
+    res = transpose(30, 31, 1, line_to_price)
+    print_matrix(res)
+    print("")
+     
+    #giovanni esterno tot
+    res = transpose(31, 32, 1, line_to_price)
+    print_matrix(res)
+    print("")
+
+    #arianna esterno x fornitori
     #res = transpose(start, end, header_row)
     res = transpose(32, 37, 1, line_to_price)
-    #print(res)
-    for r in res:
-        print(r)
-        for price in res[r]:
-            print(" > {}".format(price))
-
+    print_matrix(res)
+    print("")
+    
+    #giovanni esterno x fornitori
+    #res = transpose(start, end, header_row)
+    res = transpose(37, 39, 1, line_to_price)
+    print_matrix(res)
+    print("")
+    
+    #arianna interno tot
+    res = transpose(40, 41, 1, line_to_price)
+    print_matrix(res)
+    print("")
+     
+    #giovanni interno tot
+    res = transpose(41, 42, 1, line_to_price)
+    print_matrix(res)
+    print("")
+    
+    #giovanni interno x fornitori
+    #res = transpose(start, end, header_row)
+    res = transpose(42, 46, 1, line_to_price)
+    print_matrix(res)
+    print("")
+    
+    #arianna interno x fornitori
+    #res = transpose(start, end, header_row)
+    res = transpose(48, 50, 1, line_to_price)
+    print_matrix(res)
+    print("")
+    
 
