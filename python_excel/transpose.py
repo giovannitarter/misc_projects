43dc687262abf3fd010ecb4d60c524ba603d70d8
Giovanni Tarter <giovanni.tarter@praim.com>
2019-09-05 18:23:54 +0200

    modified:   transpose.py

--- python_excel/transpose.py
+++ python_excel/transpose.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python
 
 import xlrd
 import re
@@ -13,10 +14,10 @@ class Price(object):
         self.start_line = start_row
         self.name = name
         self.costs = []
-   
+
 
 def parse_categories(sh):
-   
+
     int_cat = {}
     ext_cat = {}
 
@@ -30,19 +31,19 @@ def parse_categories(sh):
         cell = row[idx]
         if cell.ctype != 0:
             ext_cat[idx] = cell.value
-    
+
     print("")
     print(int_cat)
     print(ext_cat)
 
     return int_cat, ext_cat
-    
+
 
 def transpose(start, end, header_row, line_to_price):
-    
+
     headers = {}
     cat_to_prices = {}
-    
+
     for cat in range(start, end):
         header = sh.cell(header_row, cat).value
         header.replace("\n", " ")
@@ -50,7 +51,7 @@ def transpose(start, end, header_row, line_to_price):
 
         prices_list = cat_to_prices.get(cat, [])
         cat_to_prices[cat] = prices_list
-    
+
         for idx, cell in enumerate(sh.col(cat)):
 
             #we want indexes to match excel lines (starting with 1)
@@ -98,10 +99,10 @@ def parse_prices(sheet):
             continue
 
         if l.ctype != 0:
-            
+
             ##cerca codice prezzo su prima colonna e imposta cprice
             if re.match(
-                    "[A-Z]\.[0-9]+\.[0-9]+\.[0-9]+[.0-9]*\**$", 
+                    "[A-Z]\.[0-9]+\.[0-9]+\.[0-9]+[.0-9]*\**$",
                     l.value
                     ) is not None:
 
@@ -110,7 +111,7 @@ def parse_prices(sheet):
                 description = sh.cell(idx, 2).value
                 c_price = Price(l.value, idx, name, description)
                 prices.append(c_price)
-                
+
                 rows = []
                 for idx2 in range(3, 25):
                     row = sh.row(idx + idx2)
@@ -124,16 +125,16 @@ def parse_prices(sheet):
 #                print("working on idx: {}".format(idx))
 #                for idx, r in enumerate(rows):
 #                    print("{} - {}\n".format(idx, r))
-                
+
                 for row_nr, r in rows:
                     price_cells = r[13:15]
-                    
+
                     cost_type = "int"
                     if r[13].ctype == 2:
                         cost_type = "est"
                     else:
                         cost_type = "int"
-    
+
                     price_cells = [ x for x in price_cells if x.ctype != 0 ]
                     if price_cells != []:
                         if price_cells[0].ctype == 2:
@@ -141,9 +142,9 @@ def parse_prices(sheet):
                             if val != 0:
                                 c_price.costs.append(
                                         (
-                                            row_nr, 
-                                            r[1].value, 
-                                            cost_type, 
+                                            row_nr,
+                                            r[1].value,
+                                            cost_type,
                                             val
                                             )
                                         )
@@ -152,19 +153,19 @@ def parse_prices(sheet):
 
 
 if __name__ == "__main__":
-    
+
     book = xlrd.open_workbook(sys.argv[1])
     print("The number of worksheets is {0}".format(book.nsheets))
     print("Worksheet name(s): {0}".format(book.sheet_names()))
     sh = book.sheet_by_index(6)
 
     int_cat, ext_cat = parse_categories(sh)
-    
+
     all_cat = {}
     all_cat.update(int_cat)
     all_cat.update(ext_cat)
 
-    
+
     prices = parse_prices(sh)
 
     tot_int = 0
@@ -201,7 +202,7 @@ if __name__ == "__main__":
     #for cat in int_cat:
     #    prices_list = cat_to_prices.get(cat, [])
     #    cat_to_prices[cat] = prices_list
-    #    
+    #
     #    for idx, cell in enumerate(sh.col(cat)):
     #        if cell.ctype == 2:
     #            if idx in line_to_price.keys():
@@ -210,7 +211,7 @@ if __name__ == "__main__":
     #for cat in ext_cat:
     #    prices_list = cat_to_prices.get(cat, [])
     #    cat_to_prices[cat] = prices_list
-    #    
+    #
     #    for idx, cell in enumerate(sh.col(cat)):
     #        if cell.ctype == 2:
     #            if idx in line_to_price.keys():
@@ -226,52 +227,52 @@ if __name__ == "__main__":
     #cat_contrib = sh.row(1)
     #for idx, c in enumerate(cat_contrib):
     #    print("{} -> {}".format(idx, c.value))
-        
+
 
     #tot_gio = #row 30
     #tot_ari = #row 31
-    
+
     #esterno
     res = transpose(16, 21, 0, line_to_price)
     pretty_print_matrix("ESTERNO", res)
-    
+
     #interno
     res = transpose(22, 28, 0, line_to_price)
-    print_matrix(res)
-    
+    pretty_print_matrix("INTERNO", res)
+
     #arianna esterno tot
     res = transpose(30, 31, 1, line_to_price)
-    print_matrix(res)
-     
+    pretty_print_matrix("INTERNO", res)
+
     #giovanni esterno tot
     res = transpose(31, 32, 1, line_to_price)
-    print_matrix(res)
+    pretty_print_matrix("GIOVANNI - ESTERNO", res)
 
     #arianna esterno x fornitori
     #res = transpose(start, end, header_row)
     res = transpose(32, 37, 1, line_to_price)
-    print_matrix(res)
-    
+    pretty_print_matrix("ARIANNA - ESTERNO - FORNITORI", res)
+
     #giovanni esterno x fornitori
     #res = transpose(start, end, header_row)
     res = transpose(37, 39, 1, line_to_price)
-    print_matrix(res)
-    
+    pretty_print_matrix("GIOVANNI - ESTERNO - FORNITORI", res)
+
     #arianna interno tot
     res = transpose(40, 41, 1, line_to_price)
-    print_matrix(res)
-     
+    pretty_print_matrix("ARIANNA - INTERNO", res)
+
     #giovanni interno tot
     res = transpose(41, 42, 1, line_to_price)
-    print_matrix(res)
-    
+    pretty_print_matrix("GIOVANNI - INTERNO", res)
+
     #giovanni interno x fornitori
     #res = transpose(start, end, header_row)
     res = transpose(42, 46, 1, line_to_price)
-    print_matrix(res)
-    
+    pretty_print_matrix("GIOVANNI - INTERNO - FORNITORI", res)
+
     #arianna interno x fornitori
     #res = transpose(start, end, header_row)
     res = transpose(48, 50, 1, line_to_price)
-    print_matrix(res)
+    pretty_print_matrix("GIOVANNI - INTERNO - FORNITORI", res)
 
